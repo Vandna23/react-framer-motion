f0356779541c0442d3084566866796bc0b5b5c14
Vandna <vandna@WILASSETA0190.local>
2024-11-06 15:22:59 +0530

    initail commit

--- src/fifth.js
+++ src/fifth.js
@@ -1,25 +1,30 @@
-
-import * as React from 'react'
-import { useState } from 'react'
-import { createRoot } from 'react-dom/client'
-import { screen, fireEvent } from '@testing-library/dom'
+import * as React from "react";
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { screen, fireEvent } from "@testing-library/dom";
 
 function App() {
-  const [state, setState] = useState(0)
-  console.log("App " + state)
+  const [state, setState] = useState(0);
+  console.log("App " + state);
   return (
     <div>
-      <button onClick={() => {
-        setState(count => count + 1)
-        setState(count => count * 2)
-      }}>click me</button>
+      <button
+        onClick={() => {
+          setState((count) => count + 1);
+          setState((count) => count * 2);
+        }}
+      >
+        click me
+      </button>
     </div>
-  )
+  );
 }
+console.log(
+  "first",
+  first
+)(async () => {
+  const root = createRoot(document.getElementById("root"));
+  root.render(<App />);
 
-(async () => {
-  const root = createRoot(document.getElementById('root'));
-  root.render(<App/>)
-
-  fireEvent.click(await screen.findByText('click me'))
-})()
+  fireEvent.click(await screen.findByText("click me"));
+})();
